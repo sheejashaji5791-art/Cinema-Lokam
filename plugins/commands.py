ad0e34f9e871e263a3a82d17b56c85e1c737aee4
Weegg <aswanthshaji2009@gmail.com>
2025-12-18 10:41:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -94,17 +94,12 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-                    InlineKeyboardButton('+ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ +', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
-                ],[
-                    InlineKeyboardButton('🧧 ᴛʀᴇɴᴅɪɴɢ', callback_data="topsearch"),
-                    InlineKeyboardButton('🎟️ ᴜᴘɢʀᴀᴅᴇ', callback_data="premium"),
-                ],[
-                    InlineKeyboardButton('♻️ ᴅᴍᴄᴀ', callback_data='disclaimer'),
-                    InlineKeyboardButton('👤 ᴀʙᴏᴜᴛ', callback_data='me')
-                ],[
-                    InlineKeyboardButton('🚫 ᴇᴀʀɴ ᴍᴏɴᴇʏ ᴡɪᴛʜ ʙᴏᴛ 🚫', callback_data="earn")
-                ]]
+        buttons = [
+            [InlineKeyboardButton('🍿 𝑀𝑜𝑣𝑖𝑒 𝐺𝑟𝑜𝑢𝑝 🍿', url='https://t.me/Cinema_Lokam_Movies')],
+            [InlineKeyboardButton('⇉ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ ⇇', url=f'http://t.me/{temp.U_NAME}?startgroup=true')],
+            [InlineKeyboardButton('🖥️Oᴛᴛ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ🖥️', url='https://t.me/Cinema_Updates_OTT')]
+            [InlineKeyboardButton('🛠 𝘈𝘣𝘰𝘶𝘵  🛠', callback_data='me'),
+		]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
