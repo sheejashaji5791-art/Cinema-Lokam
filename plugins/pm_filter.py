e102513077df5b0bd79bbc98ceb53e3f124d9358
Weegg <aswanthshaji2009@gmail.com>
2025-12-18 23:07:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -892,11 +892,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.edit_text("● ◌ ◌")
         await query.message.edit_text("● ● ◌")
         await query.message.edit_text("● ● ●")
-            await query.message.edit_text(
-                text=(script.DISCLAIMER_TXT),
-                reply_markup=reply_markup,
-                parse_mode=enums.ParseMode.HTML 
-			)
+        await query.message.edit_text(
+            text=(script.DISCLAIMER_TXT),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML 
+		)
     elif query.data == "system":
         buttons = [[
             InlineKeyboardButton('Dᴇᴛᴀɪʟs', callback_data='me'),
