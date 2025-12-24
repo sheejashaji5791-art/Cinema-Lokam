5f133d0325334e513a963ec0013d6d3dc9569caa
sheejashaji5791-art <sheejashaji5791@gmail.com>
2025-12-24 18:30:48 +0530

    Fix button label formatting in commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,7 +98,8 @@ async def start(client, message):
             [InlineKeyboardButton('🍿 𝑀𝑜𝑣𝑖𝑒 𝐺𝑟𝑜𝑢𝑝 🍿', url='https://t.me/Cinema_Lokam_Movies')],
             [InlineKeyboardButton('⇉ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ ⇇', url=f'http://t.me/{temp.U_NAME}?startgroup=true')],
             [InlineKeyboardButton('🖥️Oᴛᴛ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ🖥️', url='https://t.me/Cinema_Updates_OTT')],
-            [InlineKeyboardButton('🛠 𝘈𝘣𝘰𝘶𝘵  🛠', callback_data='me')]
+            [InlineKeyboardButton('🛠 Aʙᴏᴜᴛ  🛠', callback_data='me')]
+			[InlineKeyboardButton('🎁 𝑪𝒍𝒐𝒏𝒆 🪩', callback_data='about')]
 		]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
