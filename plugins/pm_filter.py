649db76a9cde28e5f00b08d313ee9a8d82dff995
sheejashaji5791-art <sheejashaji5791@gmail.com>
2025-12-24 19:12:11 +0530

    Fix formatting in InlineKeyboardButton list

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1313,7 +1313,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             [InlineKeyboardButton('🍿 𝑀𝑜𝑣𝑖𝑒 𝐺𝑟𝑜𝑢𝑝 🍿', url='https://t.me/Cinema_Lokam_Movies')],
             [InlineKeyboardButton('⇉ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ ⇇', url=f'http://t.me/{temp.U_NAME}?startgroup=true')],
             [InlineKeyboardButton('🖥️Oᴛᴛ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ🖥️', url='https://t.me/Cinema_Updates_OTT')],
-            [InlineKeyboardButton('🛠 Aʙᴏᴜᴛ  🛠', callback_data='me')]
+            [InlineKeyboardButton('🛠 Aʙᴏᴜᴛ  🛠', callback_data='me')],
 			[InlineKeyboardButton('🎁 𝑪𝒍𝒐𝒏𝒆 🪩', callback_data='about')]
 		]
         reply_markup = InlineKeyboardMarkup(buttons)
